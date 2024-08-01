12d1aaf495dcfa4bfc8aae5ab6ff35e549d5fa29
123-123268 <kavyayadav1980@gmail.com>
2024-08-01 15:13:28 +0530

    storeContext.jsx done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
-import Foodcard from './components/Foodcard'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-    {/* <Navbar/>
-    <Header/> */}
-    <Foodcard/>
+    <Navbar/>
+    <Header/>
+    <Footer/>
     </>
   )
 }
