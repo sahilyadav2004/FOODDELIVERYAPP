be685fac246bbc7da428b08c41b2d0c50ecadc67
sahilyadav2004 <sahillawaniya4@gmail.com>
2024-08-01 14:39:22 +0530

    assist file added

--- frontend/src/assets/Assets.js
+++ frontend/src/assets/Assets.js
@@ -1,1 +1,345 @@
-const Foodlist=[]
+import alooParantha from './alooParantha.jpeg'
+import alootikkiburger from './alootikkiburger.jpg'
+import blackforest from './blackforest.webp'
+import blueberryCake from './blueberryCake.jpg'
+import cheeseSandwich from "./cheeseSandwich.jpeg"
+import chocolateShake from './chocolateShake.jpg'
+import cholaSamosa from './cholaSamosa.jpg'
+import extraChezeePizza from './exraChezeePizza.jpeg'
+import extraCheezeburger from './extraCheezeburger.jpeg'
+import farmFreshPizza from './farmFreshPizza.png'
+import icecreamMilkShake from './icecreamMilkShake.jpg'
+import jalepenoPizza from "./jalepenoPizza.jpg"
+import mangoShake from './mangoShake.png'
+import matkavegbiryani from './matkavegbiryani.jpg'
+import mayoSandwich from './mayoSandwich.jpg'
+import methiParatha from './methiParatha.jpg'
+import noddleRoll from './noddleRoll.jpg'
+import normalparatha from './normalparatha.jpg'
+import oreoCake from './oreoCake.jpg'
+import oreoShake from "./oreoShake.jpeg"
+import paneerParatha from "./paneerParatha.jpeg"
+import paneersamosa from "./paneersamosa.jpeg"
+import paneerSandwich from "./paneerSandwich.jpeg"
+import samosa from "./samosa"
+import springRoll from "./springRoll.jpeg"
+import strawberryCake from "./strawberryCake.jpeg"
+import vadaPav from "./vadaPav.jpg"
+import vanillaCake from "./vanillaCake.jpeg"
+import veggieBurger from "./veggieBurger.jpg"
+import veggieRoll from "./veggieRoll.jpg"
+import veggieDelight from "./veggieDelight.jpeg"
+import veggieSandwich from "./veggieSandwich.jpg"
+
+
+export const assets={
+    alootikkiburger,
+    alooParantha,
+    blackforest,
+    blueberryCake,
+    cheeseSandwich,
+    chocolateShake,
+    cholaSamosa,
+    extraCheezeburger,
+    extraChezeePizza,
+    farmFreshPizza,
+    icecreamMilkShake,
+    jalepenoPizza,
+    mangoShake,
+    matkavegbiryani,
+    mayoSandwich,
+    methiParatha,
+    noddleRoll,
+    normalparatha,
+    oreoCake,
+    oreoShake,
+    paneerParatha,
+    paneersamosa,
+    paneerSandwich,
+    samosa,
+    springRoll,
+    strawberryCake,
+    vadaPav,
+    vanillaCake,
+    veggieBurger,
+    veggieRoll,
+    veggieDelight,
+    veggieSandwich
+}
+
+
+
+export const menu_list = [
+    {
+        menu_name: "Cake",
+        menu_image: blackforest
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: veggieRoll
+    },
+    {
+        menu_name: "Pizza",
+        menu_image: farmFreshPizza
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: paneerSandwich
+    },
+    {
+        menu_name: "Burger",
+        menu_image: veggieBurger
+    },
+    {
+        menu_name: "Samosa",
+        menu_image: samosa
+    },
+    {
+        menu_name: "Paratha",
+        menu_image: methiParatha
+    },
+    {
+        menu_name: "Shake",
+        menu_image: oreoShake
+    },
+    {
+        menu_name:"Biryani",
+        menu_image: veggieBiryani
+    }
+]
+
+
+
+    export const food_list = [
+        {
+            _id: "1",
+            name: "Aloo Tikki Burger",
+            image: alootikkiburger,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Burger"
+        },
+        {
+            _id: "2",
+            name: "Extra Cheese Burger",
+            image: extraCheezeburger,
+            price: 18,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Burger"
+        }, {
+            _id: "3",
+            name: "Veggie Burger",
+            image: veggieBurger,
+            price: 16,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Burger"
+        }, {
+            _id: "4",
+            name: "Vada Pav",
+            image: vadaPav,
+            price: 24,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Burger"
+        }, {
+            _id: "5",
+            name: "Spring Roll",
+            image: springRoll,
+            price: 14,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Rolls"
+        }, {
+            _id: "6",
+            name: "Veggie Roll",
+            image: veggieRoll,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Rolls"
+        }, {
+            _id: "7",
+            name: "Noddle Roll",
+            image: noddleRoll,
+            price: 20,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Rolls"
+        }, {
+            _id: "8",
+            name: "Jalepano ",
+            image: jalepenoPizza,
+            price: 15,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pizza"
+        }, {
+            _id: "9",
+            name: "Extra Cheese Pizza",
+            image: extraChezeePizza,
+            price: 14,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pizza"
+        }, {
+            _id: "10",
+            name: "farm Fresh Pizza",
+            image: farmFreshPizza,
+            price: 22,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pizza"
+        }, {
+            _id: "11",
+            name: "Veggie Delight",
+            image: veggieDelight,
+            price: 10,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Pizza"
+        }, {
+            _id: "12",
+            name: "Cheese Sandwich",
+            image:cheeseSandwich,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Sandwich"
+        },
+        {
+            _id: "13",
+            name: "Mayo Sandwich",
+            image: mayoSandwich,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Sandwich"
+        },
+        {
+            _id: "14",
+            name: "Paneer Sandwich",
+            image: paneerSandwich,
+            price: 18,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Sandwich"
+        }, {
+            _id: "15",
+            name: "Veggie Sandwich",
+            image: veggieSandwich,
+            price: 16,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Sandwich"
+        }, {
+            _id: "16",
+            name: "Samosa",
+            image: samosa,
+            price: 24,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Samosa"
+        }, {
+            _id: "17",
+            name: "Paneer Samosa",
+            image: paneersamosa,
+            price: 14,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Samosa"
+        }, {
+            _id: "18",
+            name: "Chola Samosa",
+            image: cholaSamosa,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Samosa"
+        }, {
+            _id: "19",
+            name: "Oreo Cake",
+            image: oreoCake,
+            price: 20,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Cake"
+        }, {
+            _id: "20",
+            name: "Blackforest Cake",
+            image: blackforest,
+            price: 15,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Cake"
+        }, {
+            _id: "21",
+            name: "Bluebeery Cake ",
+            image: blueberryCake,
+            price: 14,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Cake"
+        }, {
+            _id: "22",
+            name: "Strawberry Cake",
+            image: strawberryCake,
+            price: 22,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Cake"
+        }, {
+            _id: "23",
+            name: "Vanilla Cake",
+            image: vanillaCake,
+            price: 10,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Cake"
+        }, {
+            _id: "24",
+            name: "Allo Parantha",
+            image: alooParantha,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Paratha"
+        },
+        {
+            _id: "25",
+            name: "Paneer Paratha",
+            image: paneerParatha,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Paratha"
+        },
+        {
+            _id: "26",
+            name: "Methi Parantha",
+            image: methiParatha,
+            price: 18,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Paratha"
+        }, {
+            _id: "27",
+            name: "Normal Paratha",
+            image: normalparatha,
+            price: 16,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Paratha"
+        }, {
+            _id: "28",
+            name: "Choclate Shake",
+            image: chocolateShake,
+            price: 24,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Shake"
+        }, {
+            _id: "29",
+            name: "Ice Cream Shake",
+            image: icecreamMilkShake,
+            price: 14,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Shake"
+        }, {
+            _id: "30",
+            name: "Mango Shake",
+            image: mangoShake,
+            price: 12,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Shake"
+        }, {
+            _id: "31",
+            name: "Oreo Shake",
+            image: oreoShake,
+            price: 20,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Shake"
+        },
+        {
+            _id: "32",
+            name: "Veg Biryani",
+            image: veggieBiryani,
+            price: 20,
+            description: "Food provides essential nutrients for overall health and well-being",
+            category: "Biryani"
+        }
+    ]
